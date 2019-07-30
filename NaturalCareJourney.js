857316daeddb5cd7a349881aa40c131b493dad0e
unknown <nehemieyacinthe@gmail.com>
2019-07-30 14:14:14 -0400

    initial commit

---
+++ NaturalCareJourney.js
@@ -0,0 +1,80 @@
+function myFunction() {
+  var x = document.getElementById("dairy");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function sugar() {
+  var x = document.getElementById("sugar");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function carbs() {
+  var x = document.getElementById("refined carbs");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function hgf() {
+  var x = document.getElementById("hgf");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function coffee() {
+  var x = document.getElementById("coffee");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function zinc() {
+  var x = document.getElementById("zinc");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function omega() {
+  var x = document.getElementById("omega");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function vitc() {
+  var x = document.getElementById("vitc");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function vite() {
+  var x = document.getElementById("vite");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}function vita() {
+  var x = document.getElementById("vita");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
